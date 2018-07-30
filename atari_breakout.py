0aaadb070b36e9a2028f79e608842f3372697a05
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-07-30 14:22:26 -0400

    add paddle

--- atari_breakout.py
+++ atari_breakout.py
@@ -8,3 +8,23 @@ import pygame
 from pygame.locals import *
 
 pygame.init()
+width = 1920
+height = 1080
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+red = (255, 0, 0)
+
+screen = pygame.display.set_mode((width, height))
+screen.fill(black)
+pygame.display.set_caption('Atari Breakout')
+clock = pygame.time.Clock()
+
+while True:
+    if pygame.event.get(pygame.QUIT):
+        pygame.quit()
+        sys.exit()
+    paddle = pygame.draw.rect(screen, white, [width / 2, 800, 300, 30])
+    pygame.display.flip()
+
+    clock.tick(60)
